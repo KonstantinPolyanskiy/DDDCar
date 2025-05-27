bd3ad2080e96a12a3b900c7638cb82bf9271a4be
k.polyanskiy <k.polyanskiy@qpdev.ru>
2025-05-27 18:41:14 +0300

    ddd

---
+++ DDDCar.Core/DomainObjects/Car.cs
@@ -0,0 +1,27 @@
+﻿namespace DDDCar.Core.DomainObjects;
+
+/// <summary> Цвет машины </summary>
+public enum Colors
+{
+    Black,
+    Blue,
+    Green,
+    White,
+}
+
+/// <summary> Состояние машины </summary>
+public enum Condition
+{
+    New,
+    Used,
+    NotWorking
+}
+
+public class Car
+{
+    public string? Brand { get; set; }
+    
+    public Colors? Color { get; set; }
+    
+    public Condition? Condition { get; set; }
+}
